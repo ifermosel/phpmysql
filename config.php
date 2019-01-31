454b0c8bbeac52d83ee8caeabc5eec2b25678eeb
Ismael <30444235+ifermosel@users.noreply.github.com>
2019-01-31 10:47:46 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootroot');
+   define('DB_PASSWORD', '');
    define('DB_DATABASE', 'pedidos');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
